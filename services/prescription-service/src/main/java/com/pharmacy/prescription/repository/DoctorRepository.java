a80d24a077c935bdca50ba0c8eb70898966d2e8b
Niall Roche <niall.roche03@hotmail.com>
2026-04-13 13:17:25 +0100

    [Prescription] Add Doctor domain model to prescription service

---
+++ services/prescription-service/src/main/java/com/pharmacy/prescription/repository/DoctorRepository.java
@@ -0,0 +1,12 @@
+package com.pharmacy.prescription.repository;
+
+import com.pharmacy.prescription.entity.Doctor;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface DoctorRepository extends JpaRepository<Doctor, UUID> {
+    Optional<Doctor> findByEmail(String email);
+    boolean existsByLicenseNumber(String licenseNumber);
+}
